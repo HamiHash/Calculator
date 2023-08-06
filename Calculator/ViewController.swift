f6224e0824b2a1dfaf93f23cc043d980b5b45bb9
hamihash <hami.hash@yahoo.com>
2023-08-07 01:24:44 +0330

    add method functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,28 +13,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     private var isFinishedTypingNumber: Bool = true
-
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
-
+        
+        // number in calculator
+        let number = Double(displayLabel.text!)!
+        
+        // methods
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            }
+            
+            if calcMethod == "AC" {
+                displayLabel.text = String(0)
+            }
+            
+            if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
+        
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         //What should happen when a number is entered into the keypad
+        
+        // if value is not
         if let numValue = sender.currentTitle {
-            
             if isFinishedTypingNumber {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
                 displayLabel.text?.append(numValue)
             }
-    
         }
-    
+        
     }
-
+    
 }
-
